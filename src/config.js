f11caeb975ae0ad20b75b9388641c285753cca2e
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-29 12:24:18 -0400

    Add Express.js

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const {Joi} = require('celebrate')
+
+const envVarsSchema = Joi.object().keys({
+  NODE_ENV: Joi.string().valid('development', 'test', 'production').required(),
+  PORT: Joi.number().integer().required()
+}).unknown()
+
+const {value: envVars, error} = envVarsSchema.validate(process.env)
+if (error) {
+  throw error
+}
+
+const config = {
+  environment: envVars.NODE_ENV,
+  port: envVars.PORT
+}
+
+module.exports = {config}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const {app} = require('./app')
+const {config} = require('./config')
+
+app.listen(config.port, () => {
+  console.log(`Users Service listening on port ${config.port}`)
+})
